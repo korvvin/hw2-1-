057a01db9b2576839422717162a2ec9c100b8790
zykov <zykov.da01@gmail.com>
2022-11-29 23:36:27 +0300

    дз первое ,задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8;
         var cat = 3.6;
-        var paper = 763788;
+        var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
